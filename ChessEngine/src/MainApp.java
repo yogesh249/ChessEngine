8b68c454daf73cb8e7af5ba287e5e156b52e464a
Yogesh Kumar Gandhi <yogesh249@gmail.com>
2018-08-18 18:23:52 +0530

    Update MainApp.java
    Updated the logic of iteration with java 8, and few code improvements.

--- ChessEngine/src/MainApp.java
+++ ChessEngine/src/MainApp.java
@@ -1,6 +1,9 @@
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class MainApp {
 
@@ -9,23 +12,23 @@ public class MainApp {
 	public MainApp(String fen) {
 		this.fen = fen;
 	}
-	// This is mate in 3
+	// This is mate in 3 in 35 sec
 //    public static String position = "2k5/1R6/2K5/8/8/8/8/8 w";
 
 //	  This is mate in 4, have solved it successfully.
-//	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
+	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
 	// This is mate in 2,
 	// With depth set to 2, it took 5 minutes to solve this puzzle correctly.
-	public static String position="2bqkbn1/2pppp2/np2N3/r3P1p1/p2N2B1/5Q2/PPPPKPP1/RNB2r2 w";
+//	public static String position="2bqkbn1/2pppp2/np2N3/r3P1p1/p2N2B1/5Q2/PPPPKPP1/RNB2r2 w";
 	public static void main(String args[]) {
 //		String position = "5rk1/6pp/4p3/pP1qPn2/P1pPN2b/7P/2Q2BP1/3R2K1 b";
 		System.out.println(new Date());
 		final ChessBoard[] cb = {new ChessBoard(position)};
 		System.out.println("After making chess board");
 		final Move[] bestMove = new Move[1];
-		int[] sc = {0};
-
+		int[] depth= {0};
 		while (!cb[0].isGameOver()) {
+			Map<Move, Integer> scoreMap = new HashMap<>();
 			int[] score = {0};
 			if (cb[0].getTurn() == Piece.WHITE) {
 				score[0] = -1000;
@@ -34,46 +37,54 @@ public class MainApp {
 			}
 
 			List<Move> moves = cb[0].getAllLegalMoves();
-//            System.out.println(moves);
-			// Iterate over all the legal moves...
-			moves.parallelStream().forEach(m-> {
-
+			Consumer<Move> c = m -> {
+				int sc=0;
 				// Evaluate only if there are more than one possible moves
-				if (moves.size() > 1) {
+				if (moves.size() >= 1) {
 					ChessBoard cb2 = cb[0].applyMove(m);
-					sc[0] = 0;
-//					 Initial depth we pass as zero to minimax function.
-					sc[0] = cb2.minimax(0);
-//                    System.out.println(":" + sc);
-					System.out.print(".");;
+					// Initial depth we pass as zero to minimax function.
+					sc = cb2.minimax(depth[0]);
+					scoreMap.put(m, sc);
+					System.out.print(".");
 				}
+				
+			};
+			moves.parallelStream().forEach(c);
+			System.out.println(scoreMap);
+			System.out.println("*******************************************************************");
+			for(Move move: moves)
+			{
+				int moveScore = scoreMap.get(move);
 				if (cb[0].getTurn() == Piece.WHITE) {
-					if (sc[0] == 1000) {
+					if (moveScore == 1000) {
 						// white is winning in this move...
-						score[0] = sc[0];
-						bestMove[0] = m;
-						return;
+						score[0] = moveScore;
+						bestMove[0] = move;
+						break;
 					}
-					if (sc[0] >= score[0]) {
-						score[0] = sc[0];
-						bestMove[0] = m;
+					if (moveScore >= score[0]) {
+						score[0] = moveScore;
+						bestMove[0] = move;
 					}
 				} else {
-					if (sc[0] <= score[0]) {
-						score[0] = sc[0];
-						bestMove[0] = m;
+					if (moveScore <= score[0]) {
+						score[0] = moveScore;
+						bestMove[0] = move;
 					}
-					if (sc[0] == -1000) {
+					if (moveScore == -1000) {
 						// Black is winning here...
-						return;
+						score[0] = moveScore;
+						bestMove[0] = move;
+						break;
 					}
 				}
-			});
-//			System.out.println(bestMove[0]);
+				
+			}
 			Piece pc = cb[0].getPiece(bestMove[0].from);
 			Point to = bestMove[0].to;
-			System.out.println(pc + "" +  to);
+			System.out.println(pc + "" + bestMove[0].from +  to +":"+ score[0]);
 			cb[0] = cb[0].applyMove(bestMove[0]);
+			depth[0]++;
 			bestMove[0] = null;
 		}
 		System.out.println(new Date());
