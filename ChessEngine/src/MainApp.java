7e730512119e4307212cb2abdfc2e5aed32d5c30
Yogesh Kumar Gandhi <yogesh249@gmail.com>
2018-08-18 15:06:12 +0530

    Update MainApp.java

--- ChessEngine/src/MainApp.java
+++ ChessEngine/src/MainApp.java
@@ -12,9 +12,11 @@ public class MainApp {
 	// This is mate in 3
 //    public static String position = "2k5/1R6/2K5/8/8/8/8/8 w";
 
-	// This is mate in 4, have solved it successfully.
-	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
-
+//	  This is mate in 4, have solved it successfully.
+//	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
+	// This is mate in 2,
+	// With depth set to 2, it took 5 minutes to solve this puzzle correctly.
+	public static String position="2bqkbn1/2pppp2/np2N3/r3P1p1/p2N2B1/5Q2/PPPPKPP1/RNB2r2 w";
 	public static void main(String args[]) {
 //		String position = "5rk1/6pp/4p3/pP1qPn2/P1pPN2b/7P/2Q2BP1/3R2K1 b";
 		System.out.println(new Date());
@@ -32,7 +34,7 @@ public class MainApp {
 			}
 
 			List<Move> moves = cb.getAllLegalMoves();
-//                    System.out.println(moves);
+//            System.out.println(moves);
 			// Iterate over all the legal moves...
 			for (Move m : moves) {
 
@@ -42,9 +44,10 @@ public class MainApp {
 //                    System.out.print("Evaluating move " + m);
 					cb2 = cb2.applyMove(m);
 					sc = 0;
-					// Initial depth we pass as zero to minimax function.
+//					 Initial depth we pass as zero to minimax function.
 					sc = cb2.minimax(0);
 //                    System.out.println(":" + sc);
+					System.out.print(".");;
 				}
 				if (cb.getTurn() == Piece.WHITE) {
 					if (sc == 1000) {
