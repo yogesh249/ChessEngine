8e35b77eb312f350e1fd9e4f27c060d11161fe1b
Yogesh Kumar Gandhi <yogesh249@gmail.com>
2018-08-23 11:11:44 +0530

    Update MainApp.java

--- ChessEngine/src/MainApp.java
+++ ChessEngine/src/MainApp.java
@@ -13,14 +13,13 @@ public class MainApp {
 	public MainApp(String fen) {
 		this.fen = fen;
 	}
-	// This is mate in 3 in 35 sec
-//    public static String position = "2K5/1r6/2k5/8/8/8/8/8 b";
+	// This is mate in 3 in 20 sec with depth 6 (with alpha beta pruning)
 //    public static String position = "2k5/1R6/2K5/8/8/8/8/8 w";
-//    public static String position="rr4k1/RR6/7K/8/8/8/8/8 w";
-//	  This is mate in 4, have solved it successfully in 5 minutes 20 seconds with depth 6
-	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
-	// This is mate in 2,
-	// With depth set to 2, it took 5 minutes to solve this puzzle correctly.
+    // Not able to solve this, with depth 12 (even with Alpha beta pruning)
+    public static String position="rr4k1/RR6/7K/8/8/8/8/8 w";
+//	  This is mate in 4, have solved it successfully in 52 seconds with depth 6 using alpha beta pruning
+//	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
+	// With depth set to 2, it took 25 seconds to solve this puzzle correctly (mate in 2) with alpha beta pruning
 //	public static String position="2bqkbn1/2pppp2/np2N3/r3P1p1/p2N2B1/5Q2/PPPPKPP1/RNB2r2 w";
 	public static void main(String args[]) {
 		System.out.println(new Date());
@@ -31,16 +30,18 @@ public class MainApp {
 		Move bestMove = null;
 		// depth is made an array, just because we need to modify it inside a lambda expression.
 		// And this is one of the ways, we can do it.
-		int[] depth= {6};
+		int[] depth= {12};
+		int[] alpha = { -Integer.MAX_VALUE };
+		int[] beta = {Integer.MAX_VALUE };
 		Double moveno=1.0;
 		int counter = 1;
 		while (!cb[0].isGameOver()) {
 			Map<Move, Integer> scoreMap = Collections.synchronizedMap(new HashMap<>());
 			int score = 0;
 			if (cb[0].getTurn() == Piece.WHITE) {
-				score = -1000;
+				score = -Integer.MAX_VALUE;
 			} else {
-				score = 1000;
+				score = Integer.MAX_VALUE;
 			}
 
 			List<Move> moves = cb[0].getAllLegalMoves();
@@ -48,9 +49,8 @@ public class MainApp {
 				int sc = 0;
 				ChessBoard cb2 = cb[0].applyMove(m);
 				// Initial depth we pass as zero to minimax function.
-				sc = cb2.minimax(depth[0]);
+				sc = cb2.minimaxWithAlphaBeta(depth[0], alpha[0], beta[0]); 
 				scoreMap.put(m, sc);
-				
 			};
 			moves.parallelStream().forEach(c);
 			for(Move move: moves)
@@ -60,11 +60,10 @@ public class MainApp {
 					score = moveScore;
 					bestMove = move;
 				}
-				
 			}
 			Piece pc = cb[0].getPiece(bestMove.from);
 			Point to = bestMove.to;
-			System.out.println(bestMove + " :" + scoreMap.get(bestMove));
+//			System.out.println(bestMove + " :" + scoreMap.get(bestMove));
 			//##################################################################################################################
 			// This logic between hashes is just for printing the moves properly....
 			if (cb[0].getTurn() == Piece.WHITE) {
