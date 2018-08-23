bbee5873c25b753f8d5e43d4157195d380416a4e
Yogesh Kumar Gandhi <yogesh249@gmail.com>
2018-08-23 12:47:06 +0530

    Update MainApp.java

--- ChessEngine/src/MainApp.java
+++ ChessEngine/src/MainApp.java
@@ -13,10 +13,10 @@ public class MainApp {
 	public MainApp(String fen) {
 		this.fen = fen;
 	}
-	// This is mate in 3 in 20 sec with depth 6 (with alpha beta pruning)
-//    public static String position = "2k5/1R6/2K5/8/8/8/8/8 w";
-    // Not able to solve this, with depth 12 (even with Alpha beta pruning)
-    public static String position="rr4k1/RR6/7K/8/8/8/8/8 w";
+	// This is mate in 3 in 12 sec with depth 6 (with alpha beta pruning)
+    public static String position = "2k5/1R6/2K5/8/8/8/8/8 w";
+//     Not able to solve this, with depth 12 (even with Alpha beta pruning)
+//    public static String position="rr4k1/RR6/7K/8/8/8/8/8 w";
 //	  This is mate in 4, have solved it successfully in 52 seconds with depth 6 using alpha beta pruning
 //	public static String position = "8/8/8/8/8/6P1/6k1/4KR1R w";
 	// With depth set to 2, it took 25 seconds to solve this puzzle correctly (mate in 2) with alpha beta pruning
@@ -28,13 +28,10 @@ public class MainApp {
 		final ChessBoard[] cb = {new ChessBoard(position)};
 		System.out.println("After making chess board");
 		Move bestMove = null;
-		// depth is made an array, just because we need to modify it inside a lambda expression.
-		// And this is one of the ways, we can do it.
-		int[] depth= {12};
+		int[] depth= {6};
 		int[] alpha = { -Integer.MAX_VALUE };
 		int[] beta = {Integer.MAX_VALUE };
-		Double moveno=1.0;
-		int counter = 1;
+		
 		while (!cb[0].isGameOver()) {
 			Map<Move, Integer> scoreMap = Collections.synchronizedMap(new HashMap<>());
 			int score = 0;
@@ -45,14 +42,12 @@ public class MainApp {
 			}
 
 			List<Move> moves = cb[0].getAllLegalMoves();
-			Consumer<Move> c = m -> {
+			moves.parallelStream().forEach(m -> {
 				int sc = 0;
 				ChessBoard cb2 = cb[0].applyMove(m);
-				// Initial depth we pass as zero to minimax function.
 				sc = cb2.minimaxWithAlphaBeta(depth[0], alpha[0], beta[0]); 
 				scoreMap.put(m, sc);
-			};
-			moves.parallelStream().forEach(c);
+			});
 			for(Move move: moves)
 			{
 				int moveScore = scoreMap.get(move);				
@@ -61,14 +56,26 @@ public class MainApp {
 					bestMove = move;
 				}
 			}
-			Piece pc = cb[0].getPiece(bestMove.from);
-			Point to = bestMove.to;
-//			System.out.println(bestMove + " :" + scoreMap.get(bestMove));
-			//##################################################################################################################
-			// This logic between hashes is just for printing the moves properly....
-			if (cb[0].getTurn() == Piece.WHITE) {
-
-				System.out.print(moveno.intValue() + ". " + pc + "" + bestMove.from + to);// +":"+ score);
+			cb[0] = cb[0].applyMove(bestMove);
+			depth[0]--;
+			bestMove = null;
+		}
+		printMoves(cb[0]);
+		System.out.println();
+		System.out.println(new Date());
+	}
+	
+	public static void printMoves(ChessBoard cb)
+	{
+		Double moveno=1.0;
+		int counter = 1;
+		ChessBoard initialPosition = new ChessBoard(position);
+		for(Move m: cb.getMoves())
+		{
+			Piece pc = initialPosition.getPiece(m.from);
+			Point to = m.to;
+			if (initialPosition.getTurn() == Piece.WHITE) {
+				System.out.print(moveno.intValue() + ". " + pc + "" + m.from + to);// +":"+ score);
 			} else {
 				if (moveno == 1.0) {
 					if (position.endsWith("b")) {
@@ -79,7 +86,7 @@ public class MainApp {
 				} else {
 					System.out.print("\t");
 				}
-				System.out.print(pc + "" + bestMove.from + to + "\n");// +":"+ score);
+				System.out.print(pc + "" + m.from + to + "\n");// +":"+ score);
 			}
 			if (position.endsWith("w")) {
 				if (counter % 2 == 0) {
@@ -90,13 +97,8 @@ public class MainApp {
 					moveno = moveno + 1;
 				}
 			}
+			initialPosition=initialPosition.applyMove(m);
 			counter++;
-			//##################################################################################################################
-			cb[0] = cb[0].applyMove(bestMove);
-			depth[0]--;
-			bestMove = null;
 		}
-		System.out.println();
-		System.out.println(new Date());
 	}
 }
